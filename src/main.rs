9b1ccab6f4164ba07ef3788105999dcb2ab5527e
Fynises <fynises.dev@proton.me>
2024-03-07 15:22:56 +0000

    added startup banners

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,36 @@ mod device_socket;
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    log::info!("starting giggletech router");
+    println!("");
+    println!("  ██████  ██  ██████   ██████  ██      ███████     ████████ ███████  ██████ ██   ██ ");
+    println!(" ██       ██ ██       ██       ██      ██             ██    ██      ██      ██   ██ ");
+    println!(" ██   ███ ██ ██   ███ ██   ███ ██      █████          ██    █████   ██      ███████ ");
+    println!(" ██    ██ ██ ██    ██ ██    ██ ██      ██             ██    ██      ██      ██   ██ ");
+    println!("  ██████  ██  ██████   ██████  ███████ ███████        ██    ███████  ██████ ██   ██ ");
+    println!("");
+    println!(" █▀█ █▀ █▀▀   █▀█ █▀█ █ █ ▀█▀ █▀▀ █▀█");
+    println!(" █▄█ ▄█ █▄▄   █▀▄ █▄█ █▄█  █  ██▄ █▀▄");
 
     // load the config, as Arc
     let application_config = Arc::new(Config::load());
+
+    println!("\n");
+    println!("\n");
+    println!(" Device Maps");
+    for (i, parameter) in application_config.proximity_parameters_multi.iter().enumerate() {
+        println!(" {} => {}", parameter.trim_start_matches("/avatar/parameters/"), application_config.headpat_device_uris[i]);
+    }
+
+    println!("\n Listening for OSC on port: {}", application_config.port_rx);
+    println!("\n Vibration Configuration");
+    println!(" Min Speed: {}%", application_config.min_speed_float);
+    println!(" Max Speed: {:?}%", application_config.max_speed_float * 100.0);
+    println!(" Scale Factor: {}%", application_config.speed_scale_float);
+    println!(" Timeout: {}s", application_config.timeout_setting);
+    println!("\nWaiting for pats...");
+
+
+
     let devices = DeviceSocket::from_config(application_config.clone()).await;
     let main_actor = Arc::new(MainActorHandle::new(devices));
 
